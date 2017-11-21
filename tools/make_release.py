71db979849ad5e02f0db8bd616da8fdbe3e9e8ac
Tom Dimiduk <tom@dimiduk.net>
2017-11-21 03:25:56 -0500

    release script

---
+++ tools/make_release.py
@@ -0,0 +1,24 @@
+import subprocess
+from shutil import rmtree
+from os import remove
+
+version = '1.3'
+rname = 'groupeng-{}'.format(version)
+
+
+subprocess.call(['git', 'clone', 'http://github.com/tdimiduk/groupeng', rname])
+def rm(f):
+    fname = '{}/{}'.format(rname, f)
+    try:
+        rmtree(fname)
+    except NotADirectoryError:
+        remove(fname)
+
+rm('.git')
+rm('.gitignore')
+rm('tests')
+
+subprocess.call(['zip', '-r', '{}.zip'.format(rname), rname])
+rmtree(rname)
+
+
